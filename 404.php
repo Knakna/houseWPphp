4bb0bd6af5e85fc8c696372a8a11925f4eb3007a
knakna <kna@tut.by>
2021-07-06 13:56:43 +0300

    add 404 page

---
+++ 404.php
@@ -0,0 +1,39 @@
+<style>
+.errow {
+    text-align: center;
+    padding: 5rem;
+    margin-top: 15rem;
+
+}
+
+.errow__color {
+    color: red;
+}
+
+.errow__text {
+    font-family: NewYork, serif;
+    font-weight: 700;
+    font-size: 11.2rem;
+    line-height: 1.4;
+    text-transform: capitalize;
+}
+
+@media (max-width: 62rem) {
+    .errow__text {
+
+        font-size: 8rem;
+    }
+}
+
+@media (max-width: 36rem) {
+    .errow__text {
+
+        font-size: 6rem;
+    }
+}
+
+</style>
+
+<div class="errow">
+<span class="errow__text">  <span class="errow__color">Ops...  </span> page not found </span>
+</div>
